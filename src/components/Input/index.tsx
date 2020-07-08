5f4f2399bd3be976b13f5daa4e4138eae54097d7
Izabela <izabela.m.alfenas@gmail.com>
2020-07-08 16:27:14 -0300

    Add initial authentication with Yup

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -28,6 +28,10 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     registerField,
   } = useField(name);
 
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
     setIsFilled(!!inputRef.current?.value);
@@ -49,7 +53,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
           {...rest}
           ref={inputRef}
           defaultValue={defaultValue}
-          onFocus={() => setIsFocused(true)}
+          onFocus={handleInputFocus}
           onBlur={handleInputBlur}
           autoComplete="off"
         />
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Form } from '@unform/web';
 import {
-  FiArrowLeft, FiMail, FiUser, FiLock,
+  FiArrowLeft,
+  FiMail,
+  FiUser,
+  FiLock,
 } from 'react-icons/fi';
+import * as Yup from 'yup';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -11,9 +15,23 @@ import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './style';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const formRef = useRef(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'Senha muito fraca'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
@@ -21,7 +39,7 @@ const SignUp: React.FC = () => {
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="nome" />
           <Input name="email" icon={FiMail} placeholder="e-mail" />
